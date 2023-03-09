1958610727488bd03c17e50e336ba597fa168c56
Brian Crisp <bcrisp084@yahoo.com>
2023-03-08 21:25:57 -0500

    Created seperate partials for the user favorites and uploads

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,19 +1,33 @@
-const submitBtn = document.querySelector("#photo-submit");
-const uploadForm = document.querySelector(".image-upload");
+$(document).ready(function () {
+  const submitBtn = document.querySelector("#photo-submit");
+  const uploadForm = document.querySelector(".image-upload");
+  const favorties = document.querySelector("#favorites");
+  const myUploadsBtn = document.querySelector(".my-uploads");
 
-function sendData(event) {
-  event.preventDefault();
-  const newPhoto = new FormData(uploadForm);
-  const profileInfo = fetch("/api/image/form", {
-    method: "POST",
-    body,
-    newPhoto,
-  });
-  if (response.ok) {
-    document.location.replace("/profile");
-  } else {
-    alert(response.stat);
+  async function sendData(event) {
+    event.preventDefault();
+    const newPhoto = new FormData(uploadForm);
+    const response = await fetch("/api/image/form", {
+      method: "POST",
+      body: newPhoto,
+    });
+    if (response.ok) {
+      console.log("response", response.status);
+      document.location.replace("/profile");
+    } else {
+      alert(response.stat);
+    }
   }
-}
+  $(".my-faves").on("click", function (event) {
+    event.preventDefault();
+    $("#profile-uploads").addClass("display-type");
+    $("#favorites").removeClass("display-type");
+  });
+  $(".my-uploads").on("click", function (event) {
+    event.preventDefault();
+    $("#profile-uploads").removeClass("display-type");
+    $("#favorites").addClass("display-type");
+  });
 
-submitBtn.addEventListener("submit", sendData);
+  submitBtn.addEventListener("submit", sendData);
+});
